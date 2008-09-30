6ca895da1581b138b81ce3f593c528529c84d9f6
orion.edwards <orion.edwards@0def81d6-793c-0410-b1de-b38ad6bc2384>
2008-09-30 03:57:33 +0000

    moar ongoing brokenness

--- FindInFiles/Finder.cs
+++ FindInFiles/Finder.cs
@@ -74,61 +74,14 @@ namespace FindInFiles
 				FileScanned(text);
 		}
 
-		/// <summary>
-		/// Stream of matching files which match options
-		/// </summary>
-		/// <param name="options">Options for where to look and what to look at</param>
-		/// <returns>A stream of fully qualified file paths which match</returns>
-		private static IEnumerable<string> FindMatchingFiles(FindFileOptions options)
-		{
-			return FindMatchingFiles(options.Directory, options);
-		}
-
-		// Internal actual recursive worker function
-		private static IEnumerable<string> FindMatchingFiles(string directory, FindFileOptions options)
-		{
-			Debug.Assert(directory != null);
-			Debug.Assert(options != null);
-
-			if (options.Directory.Length < 1)
-				throw new ArgumentException("Directory cannot be empty", directory);
-
-			if (!Directory.Exists(directory))
-				throw new ArgumentException("Directory does not exist");
-
-			// check for *.* (*'s have been stripped out so it will just be a .)
-			if (Array.Exists(options.FileExtensions, ext => ext == "."))
-			{
-				foreach (var f in Directory.GetFiles(directory).Select(file => Path.Combine(directory, file)))
-					yield return f;
-			}
-			else
-			{
-				foreach (string file in Directory.GetFiles(directory))
-				{
-					if (options.FileExtensions.Length < 1 ||
-						(Array.Exists(options.FileExtensions, ext => file.EndsWith(ext, StringComparison.CurrentCultureIgnoreCase))))
-						yield return Path.Combine(directory, file);
-				}
-			}
-
-			foreach (string dir in Directory.GetDirectories(directory))
-			{
-				if (options.DirectoryExclusions.Length < 1 ||
-					(!Array.Exists(options.DirectoryExclusions, dx => String.Compare(Path.GetFileName(dir), dx, StringComparison.CurrentCultureIgnoreCase) == 0)))
-				{
-					foreach (var f in FindMatchingFiles(dir, options))
-						yield return f;
-				}
-			}
-		}
-
 		public IEnumerable<string> Find()
 		{
 			var searcher = new Searcher(FindLineOptions);
 
+			var files = new MatchingFilePathCollection( FindFileOptions );
+
 			var results = MatchInFiles(
-				FindMatchingFiles(FindFileOptions),
+				files,
 				(lineNumber, lineText) => searcher.MatchLine(lineText) );
 
 			return new HtmlSummariser( FindLineOptions.Pattern, FindFileOptions.Directory, FindLineOptions.Replacement, results );
@@ -137,6 +90,78 @@ namespace FindInFiles
 		public IEnumerable<string> Replace()
 		{
 			return null;
+		}
+
+		class MatchingFilePathCollection : IEnumerable<string>
+		{
+			private readonly FindFileOptions Options;
+
+			public int Count { get; private set; }
+
+			public MatchingFilePathCollection( FindFileOptions options )
+			{
+				Debug.Assert( options != null );
+
+				Options = options;
+			}
+
+			public IEnumerator<string> GetEnumerator()
+			{
+				return matchFiles( Options.Directory ).GetEnumerator();
+			}
+
+			System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+			{
+				return GetEnumerator();
+			}
+
+			// Internal actual recursive worker function
+			private IEnumerable<string> matchFiles( string directory )
+			{
+				Count = 0;
+				Debug.Assert( directory != null );
+
+				if( Options.Directory.Length < 1 )
+					throw new ArgumentException( "Directory cannot be empty", directory );
+
+				if( !Directory.Exists( directory ) )
+					throw new ArgumentException( "Directory does not exist" );
+
+				// check for *.* (*'s have been stripped out so it will just be a .)
+				if( Array.Exists( Options.FileExtensions, ext => ext == "." ) )
+				{
+					foreach( var f in Directory.GetFiles( directory ).Select( file => Path.Combine( directory, file ) ) )
+					{
+						++Count;
+						yield return f;
+					}
+				}
+				else
+				{
+					foreach( string file in Directory.GetFiles( directory ) )
+					{
+						if( Options.FileExtensions.Length < 1 ||
+							(Array.Exists( Options.FileExtensions, ext => file.EndsWith( ext, StringComparison.CurrentCultureIgnoreCase ) )) )
+						{
+							++Count;
+							yield return Path.Combine( directory, file );
+						}
+					}
+				}
+
+				foreach( string dir in Directory.GetDirectories( directory ) )
+				{
+					if( Options.DirectoryExclusions.Length < 1 ||
+						(!Array.Exists( Options.DirectoryExclusions, dx => String.Compare( Path.GetFileName( dir ), dx, StringComparison.CurrentCultureIgnoreCase ) == 0 )) )
+					{
+						foreach( var f in matchFiles( dir ) )
+						{
+							++Count;
+							yield return f;
+						}
+					}
+				}
+			}
 		}
 
 		/// <summary>
