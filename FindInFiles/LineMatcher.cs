ec4138d085682bd095ff32282f837b83bc3b56d7
Adam Vandenberg <flangy@gmail.com>
2009-04-16 14:07:00 -0700

    Make LineMatcher non-static.

--- FindInFiles/LineMatcher.cs
+++ FindInFiles/LineMatcher.cs
@@ -7,37 +7,51 @@ namespace FindInFiles
 	/// <summary>
 	/// Represents all the lines in a file which match the given options
 	/// </summary>
-	static class LineMatcher
+	class LineMatcher
 	{
+	    private readonly FindLineOptions options;
+
+	    public LineMatcher(FindLineOptions options)
+        {
+            this.options = options;
+        }
+
+        public IEnumerable<Match> Filter(IEnumerable<string> filenames)
+        {
+            var scanner = options.CreateScanner();
+            bool performReplace = options.Replacement != null;
+
+            foreach (string filename in filenames)
+            {
+                bool fileWasModified = false;
+                string[] lines = File.ReadAllLines(filename); // make this read in chunks rather than all at once
+
+                // Scan each line and yield a match if found
+                for (int lineNumber = 0; lineNumber < lines.Length; ++lineNumber)
+                {
+                    int number = lineNumber;
+                    var ranges = performReplace ?
+                        scanner.ScanAndReplace(lines[lineNumber], (s) => lines[number] = s).AsCounted() :
+                        scanner.Scan(lines[lineNumber]).AsCounted();
+
+                    foreach (var range in ranges)
+                        yield return new Match(filename, lineNumber + 1, lines[lineNumber], range);
+
+                    if (ranges.Count > 0 && performReplace)
+                        fileWasModified = true;
+                }
+
+                if (fileWasModified) // write the file back to disk if we did anything
+                    File.WriteAllLines(filename, lines);
+            }
+        }
+
 	    // worker function which scans all the files and matches all the lines
-		public static IEnumerable<Match> Filter(IEnumerable<string> files, FindLineOptions options )
-		{
-			var scanner = options.CreateScanner();
-			bool performReplace = options.Replacement != null; 
-
-			foreach(string file in files)
-			{
-				bool fileWasModified = false;
-				string[] lines = File.ReadAllLines(file); // make this read in chunks rather than all at once
-				
-				// Scan each line and yield a match if found
-				for (int lineNumber = 0; lineNumber < lines.Length; ++lineNumber)
-				{
-				    int number = lineNumber;
-				    var ranges = performReplace ? 
-						scanner.ScanAndReplace(lines[lineNumber], (s) => lines[number] = s).AsCounted() :
-						scanner.Scan(lines[lineNumber]).AsCounted();
-
-					foreach( var range in ranges )
-						yield return new Match( file, lineNumber + 1, lines[lineNumber], range );
-
-				    if (ranges.Count > 0 && performReplace)
-				        fileWasModified = true;
-				}
-
-				if( fileWasModified ) // write the file back to disk if we did anything
-					File.WriteAllLines(file, lines);
-			}
-		}
+        public static IEnumerable<Match> Filter(IEnumerable<string> files, FindLineOptions options)
+        {
+            var m = new LineMatcher(options);
+            foreach (var o in m.Filter(files))
+                yield return o;
+        }
 	}
 }
