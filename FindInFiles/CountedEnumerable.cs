dc9666fd2f03bac44d146e1aaece3f1a941aa1bb
Adam Vandenberg <flangy@gmail.com>
2009-04-16 10:47:11 -0700

    Formatting changes.

--- FindInFiles/CountedEnumerable.cs
+++ FindInFiles/CountedEnumerable.cs
@@ -1,38 +1,37 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 
 namespace FindInFiles
 {
-	class CountedEnumerable<T> : IEnumerable<T>
-	{
-		private int m_count = 0;
-		private IEnumerable<T> SourceEnumerable { get; set; }
+    class CountedEnumerable<T> : IEnumerable<T>
+    {
+        private int m_count;
+        private IEnumerable<T> SourceEnumerable { get; set; }
 
-		public int Count
-		{ get { return m_count; } }
+        public int Count { get { return m_count; } }
 
-		public CountedEnumerable( IEnumerable<T> sourceEnumerable )
-		{ SourceEnumerable = sourceEnumerable; }
+        public CountedEnumerable(IEnumerable<T> sourceEnumerable)
+        { SourceEnumerable = sourceEnumerable; }
 
-		public IEnumerator<T> GetEnumerator()
-		{
-			foreach( var item in SourceEnumerable )
-			{
-				Interlocked.Increment( ref m_count );
-				yield return item;
-			}
-		}
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in SourceEnumerable)
+            {
+                Interlocked.Increment(ref m_count);
+                yield return item;
+            }
+        }
 
-		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-		{ return GetEnumerator(); }
-	}
+        IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+    }
 
-	static class IEnumerableExtensions
-	{
-		public static CountedEnumerable<T> AsCounted<T>( this IEnumerable<T> sourceEnumerable )
-		{
-			return new CountedEnumerable<T>( sourceEnumerable );
-		}
-	}
+    static class IEnumerableExtensions
+    {
+        public static CountedEnumerable<T> AsCounted<T>(this IEnumerable<T> sourceEnumerable)
+        {
+            return new CountedEnumerable<T>(sourceEnumerable);
+        }
+    }
 }
