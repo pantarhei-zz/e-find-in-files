e931691062bf0f1379d788be24765fba0d779c00
Adam Vandenberg <flangy@gmail.com>
2009-04-16 11:41:56 -0700

    Added shell for test project.

---
+++ TestFindInFiles/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace TestFindInFiles
+{
+    public class Class1
+    {
+        [Fact]
+        public void TestSomething()
+        {
+            Assert.True(true, "True");
+            Assert.True(false, "True?");
+        }
+    }
+}
