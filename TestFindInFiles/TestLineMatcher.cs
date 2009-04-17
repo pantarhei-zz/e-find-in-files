468c198a886f00e948454733d6a4fcf98dadfcda
Adam Vandenberg <flangy@gmail.com>
2009-04-16 23:53:15 -0700

    Pull out file name array.

--- TestFindInFiles/TestLineMatcher.cs
+++ TestFindInFiles/TestLineMatcher.cs
@@ -8,12 +8,14 @@ namespace TestFindInFiles
 {
     public class TestLineMatcher
     {
+        static readonly string[] files_in_files1 = new[] { @"testfiles\files1\TextFile1.txt", @"testfiles\files1\TextFile2.txt", @"testfiles\files1\TextFile3.txt" };
+
         [Fact]
         public void TestFind()
         {
             FindLineOptions o = new FindLineOptions("Woot", true, false);
             var f = new LineMatcher(o);
-            var s = new List<Match>(f.Filter(new string[] {@"testfiles\files1\TextFile1.txt",@"testfiles\files1\TextFile2.txt",@"testfiles\files1\TextFile3.txt"}));
+            var s = new List<Match>(f.Filter(files_in_files1));
             Assert.Equal(1, s.Count);
         }
     }
