6088532a46f8c72e09d8d1f97423a5b757590279
Adam Vandenberg <flangy@gmail.com>
2009-04-16 20:52:23 -0700

    Reformat HtmlOutputter

--- FindInFiles/HtmlOutputter.cs
+++ FindInFiles/HtmlOutputter.cs
@@ -5,64 +5,64 @@ using System.Text;
 namespace FindInFiles
 {
     /// <summary>
-	/// Outputs things to STDOUT in HTML format
-	/// </summary>
-	class HtmlOutputter : IOutputMatches
+    /// Outputs things to STDOUT in HTML format
+    /// </summary>
+    class HtmlOutputter : IOutputMatches
     {
-		private static string EscapeHtml(string x)
-		{
-			return x.
-				Replace("<", "&lt;").
-				Replace(">", "&gt;").
-				Replace("'", "&#39;");
-		}
+        private static string EscapeHtml(string x)
+        {
+            return x.
+                Replace("<", "&lt;").
+                Replace(">", "&gt;").
+                Replace("'", "&#39;");
+        }
 
-		/// <summary>
-		/// Returns a copy of line with HTML span tags inserted to hilight the character ranges defined by ranges
-		/// </summary>
-		/// <param name="line">The input text</param>
-		/// <param name="ranges">Character start/ends to surround in span tags</param>
-		/// <returns>A hilighted copy</returns>
-		private static string EscapedHighlight(string line, IEnumerable<IntRange> ranges)
-		{
-			var b = new StringBuilder();
-			int lastIndex = 0;
-			foreach (var r in ranges)
-			{
-				b.Append( EscapeHtml(line.Substring(lastIndex, r.Lower-lastIndex)));
-				b.Append( "<span style='background-color:yellow;'>");
-				b.Append( EscapeHtml(line.Substring(r.Lower, r.Upper-r.Lower)));
-				b.Append( "</span>");
+        /// <summary>
+        /// Returns a copy of line with HTML span tags inserted to hilight the character ranges defined by ranges
+        /// </summary>
+        /// <param name="line">The input text</param>
+        /// <param name="ranges">Character start/ends to surround in span tags</param>
+        /// <returns>A hilighted copy</returns>
+        private static string EscapedHighlight(string line, IEnumerable<IntRange> ranges)
+        {
+            var b = new StringBuilder();
+            int lastIndex = 0;
+            foreach (var r in ranges)
+            {
+                b.Append(EscapeHtml(line.Substring(lastIndex, r.Lower - lastIndex)));
+                b.Append("<span style='background-color:yellow;'>");
+                b.Append(EscapeHtml(line.Substring(r.Lower, r.Upper - r.Lower)));
+                b.Append("</span>");
 
-				lastIndex = r.Upper;
-			}
+                lastIndex = r.Upper;
+            }
 
-			b.Append( line.Substring( lastIndex ) ); //stick the trailing bit on
-			return b.ToString();
-		}
+            b.Append(line.Substring(lastIndex)); //stick the trailing bit on
+            return b.ToString();
+        }
 
-		private static string MakeShortPath(string basePath, string fullPath)
-		{
-		    if (fullPath.IndexOf(basePath, StringComparison.CurrentCultureIgnoreCase) == -1)
-		        return fullPath;
-		    return fullPath.Substring(basePath.Length);
-		}
+        private static string MakeShortPath(string basePath, string fullPath)
+        {
+            if (fullPath.IndexOf(basePath, StringComparison.CurrentCultureIgnoreCase) == -1)
+                return fullPath;
+            return fullPath.Substring(basePath.Length);
+        }
 
-	    private readonly DateTime startTime;
-		private readonly string pattern;
+        private readonly DateTime startTime;
+        private readonly string pattern;
         private readonly string directory;
         private readonly ICountedEnumerable<string> files;
         private readonly ICountedEnumerable<Match> matches;
 
-		public HtmlOutputter( string pattern, string directory, ICountedEnumerable<string> files, ICountedEnumerable<Match> matches )
-		{
-			startTime = DateTime.Now;
+        public HtmlOutputter(string pattern, string directory, ICountedEnumerable<string> files, ICountedEnumerable<Match> matches)
+        {
+            startTime = DateTime.Now;
 
-			this.pattern = pattern;
-			this.directory = directory;
-			this.files = files;
-			this.matches = matches;
-		}
+            this.pattern = pattern;
+            this.directory = directory;
+            this.files = files;
+            this.matches = matches;
+        }
 
         public void OutputHeader()
         {
@@ -70,16 +70,16 @@ namespace FindInFiles
             Console.WriteLine("<pre>");
         }
 
-        public void OutputMatch( Match match )
-		{
-			Console.WriteLine( 
-				"<a href=\"{0}\">{2}({1}): {3}</a>",
-				Util.MakeTxmtUrl(match.File, match.LineNumber),
-				match.LineNumber,
-				MakeShortPath( directory, match.File ),
-				EscapedHighlight(match.LineText, new[]{ match.Characters })
-			);
-		}
+        public void OutputMatch(Match match)
+        {
+            Console.WriteLine(
+                "<a href=\"{0}\">{2}({1}): {3}</a>",
+                Util.MakeTxmtUrl(match.File, match.LineNumber),
+                match.LineNumber,
+                MakeShortPath(directory, match.File),
+                EscapedHighlight(match.LineText, new[] { match.Characters })
+            );
+        }
 
         public void OutputFooter()
         {
